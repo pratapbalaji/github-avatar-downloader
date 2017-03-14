5d8e0f81fdb02f6e3be552205b28e3387ed5e866
Vinay Balaji <vinaybalaji@gmail.com>
2017-03-14 02:59:56 +0000

    Refactored code and added comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,15 @@
+// Link to required modules
 var request = require('request');
 var fs = require('fs');
 
+//Define Github user ID and Github Token for Authorization
 var GITHUB_USER = "vinaybalaji";
 var GITHUB_TOKEN = "5b2f9d1009d65c30fd9f0b3643d169f67a4ca6f8";
 
+//Welcome message for Program
 console.log("Welcome to the Github Avatar Downloader!");
 
+//Function to get a list of contributors (as objects) for the supplied repo owner and repo name
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var headers = {
@@ -34,6 +38,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+//Function to download avatar images for the supplied URL's and save the images to the file path specified.
 function downloadImageByURL(url, filePath) {
   var requestURL = url;
   var options = {
@@ -55,9 +60,11 @@ function downloadImageByURL(url, filePath) {
   }).pipe(fs.createWriteStream(filePath));
 }
 
+//Get command line input for repo owner name and repo name
 var owner = process.argv[2];
 var repo = process.argv[3];
 
+//Error handling if the user does not input repo owner and repo name
 if (owner && repo) {
   getRepoContributors(owner, repo, function (result) {
    downloadImageByURL(result.avatar_url, 'avatars/' + result.login + '.jpg');
